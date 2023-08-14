d32760bff8c3bc2c2e85a9dd18353303243fd485
Silvana <sil_s_s@yahoo.com.br>
2023-08-14 11:54:27 +0100

    views adjusts

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,6 @@
 import { Negociacaocontroller } from "./controllers/negociacao-controller.js";
 
 const controller = new Negociacaocontroller();
-
 const form = document.querySelector('.form');
 form.addEventListener('submit', event => {
     event.preventDefault();
---
+++ app/views/view.ts
@@ -0,0 +1,9 @@
+export class View {
+
+    protected elemento: HTMLElement;
+     
+    constructor(seletor: string) {
+        this.elemento = document.querySelector(seletor);
+    }
+
+}
