6d166b5348e3df90a50391feee1f50eb09031581
Silvana <sil_s_s@yahoo.com.br>
2023-08-23 17:04:49 +0100

    Add generic view class and methods

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,7 +1,8 @@
+import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { View } from "./view.js";
 
-export class NegociacoesView extends View {
+export class NegociacoesView extends View<Negociacoes> {
 
     template(model: Negociacoes): string {
         return `
@@ -26,12 +27,5 @@ export class NegociacoesView extends View {
             </tbody>
         </table>      
         `;
-    }
-
-    update(model: Negociacoes) : void {
-        const template = this.template(model);
-        console.log(template);
-        this.elemento.innerHTML = template;
-    }
-    
+    }    
 }
--- app/views/view.ts
+++ app/views/view.ts
@@ -1,9 +1,18 @@
-export class View {
+export class View<T> {
 
     protected elemento: HTMLElement;
      
     constructor(seletor: string) {
         this.elemento = document.querySelector(seletor);
     }
+
+    update(model: T): void { 
+        const template = this.template(model);
+        this.elemento.innerHTML = template;
+    }
+
+    template(model: T): string { 
+        throw Error('Must be implemented in the inheritance!')
+    }  
 
 }
